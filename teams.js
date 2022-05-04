310f194ab112833515c44475a84083499f35a348
Burns, Cody <cody.burns@accenture.com>
2022-05-04 16:37:00 -0500

    not force push

--- teams.js
+++ teams.js
@@ -1,8 +1,6 @@
 const { Notion: Mind } = require("@neurosity/notion");
-const { exec } = require("child_process");
+const robot = require("robotjs");
 const { email, password } = require("./auth");
-
-
 
 (async function main() {
   const mind = new Mind();
@@ -10,28 +8,27 @@ const { email, password } = require("./auth");
 
   console.log("waiting to detect mind push");
 
-  async function theForce(){
+  function theForce(){
       console.log("The force is strong with this one!!!")
     //I dont want to triger it more than once really wait at least 15 triggers before
       if (flags >= 15){
-        await command("git add .");
-        await command("git commit -m 'force push'");
-        await command("git push -u origin master -f");
+        //toggle the mute flag
+        isMute = !isMute;
+        console.log("muted: "+ isMute);
+        //toggle mute
+        robot.keyToggle("control","down","shift");
+        robot.keyTap("m");
+        robot.keyToggle("control","up","shift");
+        //set a cool down flag in case your thinking to hard
+        flags = -15;
+        };
       //if not at 15 increment and keep looking
       flags++;
-      }
   }
 
-  async function command(cmd){
-    exec (cmd,
-      (err, stdout, stderr) => {
-        console.log(err ? stderr : stdout);
-        process.exit();
-      }
-    );
-  }
-  
   var flags = 0;
+  var isMute = true;
+
   mind
     .predictions("push")
     .subscribe((prediction) => {
